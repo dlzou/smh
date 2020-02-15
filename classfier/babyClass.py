60974eb52163e1dd1a1ed7503e0db7742cd12de5
Minh Phan <phanminh6139@berkeley.edu>
2020-02-15 15:00:11 -0800

    vectorized the baby

--- classfier/babyClass.py
+++ classfier/babyClass.py
@@ -1,25 +1,28 @@
 import pandas as pd
+import numpy as np
 import librosa
 import glob
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 
-def parser(fileName):
+
+def parser(label, fileName):
     # function to load files and extract features
-    file_name = fileName;
-
+    file_name = os.path.join('./rawData/'+ str(label) + '/' + str(fileName))
     # handle exception to check if there isn't a file which is corrupted
     try:
         # here kaiser_fast is a technique used for faster extraction
-        X, sample_rate = librosa.load(fileName, res_type='kaiser_fast')
+        X, sample_rate = librosa.load(file_name, res_type='kaiser_fast')
         # we extract mfcc feature from data
         mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T, axis=0)
         feature = mfccs
     except Exception as e:
         print("Error encountered while parsing file: ", fileName)
-        return None, None
+        return []
     
-    feature = mfccs
+    feature = [label] + list(feature)
     return feature
 
 
@@ -31,11 +34,38 @@ def getFiles(mood):
             files.append(i)
     return files
 
-fullData = {}
+fullData = []
 for j in os.listdir("rawData"):
     fList = getFiles(str(j))
-    fullData[j] = fList
-print(fullData)
+    fullData.append((j,fList))
+baby = pd.DataFrame(fullData, columns = ['mood', 'file']).set_index('mood')
+
+#curFile = baby.loc['belly_pain']['file'][0]
+
+#x1 = parser('belly_pain', '549a46d8-9c84-430e-ade8-97eae2bef787-1430130772174-1.7-m-48-bp.wav')
+#print(x1)
+
+
+fileNum = 0
+soundDic = {}
+for label in baby.index:
+    for lst in baby.loc[label]:
+        for soundFile in lst:      
+           features = parser(label, soundFile)
+           soundDic[fileNum] = features
+           fileNum += 1
+
+sound = pd.DataFrame.from_dict(data = soundDic, orient = 'index',columns=['mood'] + list(range(0, 40)))
+print(sound)
+X = sound.iloc[:, 1:].values
+Y = sound.iloc[:, 0].values
+
+X_train_sound, X_test_sound, Y_train_sound, Y_test_sound = train_test_split(
+    X, Y, test_size=0.2, random_state=0)
+
+print(len(X_train_sound), len(Y_train_sound))    
+
+
 #temp = train.apply(parser, axis=1)
 #temp.columns = ['feature', 'label']
 
