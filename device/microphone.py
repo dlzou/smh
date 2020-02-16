aa7de8c1986b3f63ea8ec9b48584524845690a97
Daniel Zou <zoudl2000@gmail.com>
2020-02-16 11:48:47 -0800

    post request with file

--- device/microphone.py
+++ device/microphone.py
@@ -8,6 +8,26 @@ from aiy.voice.audio import AudioFormat, record_file, play_wav
 TEST_SOUND = '/usr/share/sounds/alsa/Front_Center.wav'
 FILENAME = 'recording.wav'
 
+# IGNORE, NOT USED
+class UniqueID:
+    def __init__(self):
+        self.active_ids = []
+
+    def get_new_id(self):
+        if len(self.active_ids) == 0:
+            self.active_ids.append(0)
+            return 0
+        for index, id in enumerate(self.active_ids):
+            if index != id:
+                self.active_ids = self.active_ids[:index] + [index] + self.active_ids[index:]
+                return index
+        self.active_ids.append(index + 1)
+        return index + 1
+
+    def release_id(self, id):
+        self.active_ids.remove(id)
+
+
 def main():
     print('Playing test sound...')
     play_wav(TEST_SOUND)
@@ -22,21 +42,16 @@ def main():
             done = threading.Event()
             board.button.when_pressed = done.set
 
-            # def wait():
-            #     start = time.monotonic()
-            #     while not done.is_set():
-            #         duration = time.monotonic() - start
-            #         print('Recording: %.02f seconds [Press button to stop]' % duration)
-            #         time.sleep(0.5)
-
             record_file(AudioFormat.CD, filename=FILENAME, wait=wait(done), filetype='wav')
 
             # run classifier
-            state = 'hungry'
-            print(state)
-            payload = {'type': state}
-            headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
-            r = requests.post('http://bigrip.ocf.berkeley.edu:5000/notify', json=payload, headers=headers)
+            # state = 'hungry'
+            # print(state)
+            # payload = {'type': state}
+            # headers = {'Accept': 'application/json', 'Content-Type': 'application/json'}
+            # r = requests.post('http://bigrip.ocf.berkeley.edu:5000/notify', json=payload, headers=headers)
+            with open(FILENAME, 'rb') as file:
+                r = requests.post('http://bigrip.ocf.berkeley.edu:5000/sendaudio', data=file)
             print(r.status_code)
 
 def wait(done):
